c2cd28196c9ec871d0adeb47e608cdccfdd358fb
Arunkumar Ekambaram <arunkumar.e@outlook.com>
2020-10-06 15:48:28 +0530

    Add project files.

---
+++ BakeryAPI/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using BakeryAPI.Models;
+
+namespace BakeryAPI.Controllers
+{
+    [RoutePrefix("api/Bakery")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ProductsController : ApiController
+    {
+        private BakeryAppEntities _bakeryApp = null;
+
+        public ProductsController()
+        {
+            _bakeryApp = new BakeryAppEntities();
+        }
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _bakeryApp.Products.ToList();
+        }
+
+        [HttpPost]
+        [Route("AddNew")]
+        public void AddNewProduct([FromBody] Product product)
+        {
+            try
+            {
+                _bakeryApp.Products.Add(product);
+                _bakeryApp.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
